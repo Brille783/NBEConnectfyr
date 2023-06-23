999c9d844bbe8ef1cfebac458d7c0dfe06f639ed
adhocit <svj@adhocit.dk>
2023-06-23 13:40:51 +0200

    another attempt to fix labels

--- custom_components/NBEConnect/config_flow.py
+++ custom_components/NBEConnect/config_flow.py
@@ -18,17 +18,26 @@ class NbeConnectConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(title="NBEConnect by svj", data=user_input)
 
         # Load the labels from strings.json
-        labels = await self.hass.async_add_executor_job(
+        strings = await self.hass.async_add_executor_job(
             self.hass.config.path, "strings.json"
         )
 
-        # Show the configuration form to the user with labels
+      
+        # Extract the title and fields from the strings
+        title = strings.get("title")
+        fields = strings.get("config", {}).get("fields", {})
+
+        # Create the data schema with labels
+        data_schema = vol.Schema({
+            vol.Required("serial", description={"label": fields.get("serial", {}).get("label")}): str,
+            vol.Required("password", description={"label": fields.get("password", {}).get("label")}): str,
+            vol.Optional("ip_address", description={"label": fields.get("ip_address", {}).get("label")}): str,
+        })
+
+        # Show the configuration form to the user with title and labels
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("serial", description={"label": labels["config"]["fields"]["serial"]["label"]}): str,
-                vol.Required("password", description={"label": labels["config"]["fields"]["password"]["label"]}): str,
-                vol.Optional("ip_address", description={"label": labels["config"]["fields"]["ip_address"]["label"]}): str,
-            }),
+            data_schema=data_schema,
+            description_placeholders={"title": title},
             errors=errors
         )
