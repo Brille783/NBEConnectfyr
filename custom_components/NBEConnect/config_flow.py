822daf0bcbb64b70ccf0e46498b901daf2e8b203
adhocit <svj@adhocit.dk>
2023-06-23 13:27:06 +0200

    Attempt to fix missing config_flow labels

--- custom_components/NBEConnect/config_flow.py
+++ custom_components/NBEConnect/config_flow.py
@@ -1,8 +1,8 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, DATA_SCHEMA
+from .const import DOMAIN
 
-class NbertbConfigFlow(config_entries.ConfigFlow, domain = DOMAIN):
+class NbeConnectConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
 
@@ -15,11 +15,20 @@ class NbertbConfigFlow(config_entries.ConfigFlow, domain = DOMAIN):
 
             if not errors:
                 # Configuration is valid, create the entry
-                return self.async_create_entry(title="NBEConnect by SVANGGAARD", data=user_input)
+                return self.async_create_entry(title="NBEConnect by svj", data=user_input)
 
-        # Show the configuration form to the user
+        # Load the labels from strings.json
+        labels = await self.hass.async_add_executor_job(
+            self.hass.config.path, "strings.json"
+        )
+
+        # Show the configuration form to the user with labels
         return self.async_show_form(
             step_id="user",
-            data_schema=DATA_SCHEMA,
+            data_schema=vol.Schema({
+                vol.Required("serial", description={"label": labels["config"]["fields"]["serial"]["label"]}): str,
+                vol.Required("password", description={"label": labels["config"]["fields"]["password"]["label"]}): str,
+                vol.Optional("ip_address", description={"label": labels["config"]["fields"]["ip_address"]["label"]}): str,
+            }),
             errors=errors
         )
