113f92484a95942ebc808cd3e2d8e1857f132e80
adhocit <svj@adhocit.dk>
2023-06-26 00:33:18 +0200

    Setting sensor unique IDs explicitly

--- custom_components/NBEConnect/sensor.py
+++ custom_components/NBEConnect/sensor.py
@@ -29,13 +29,13 @@ async def async_setup_entry(hass, entry, async_add_entities):
     dc = hass.data[DOMAIN][entry.entry_id+'_coordinator']
 
     async_add_entities([
-        RTBBinarySensor(dc, 'Boiler Running', 'operating_data/power_pct', BinarySensorDeviceClass.HEAT),
-        RTBBinarySensor(dc, 'Boiler Alarm', 'operating_data/off_on_alarm', BinarySensorDeviceClass.PROBLEM),
-        RTBSensor(dc, 'Boiler Temperature', 'operating_data/boiler_temp', SensorDeviceClass.TEMPERATURE),
-        RTBSensor(dc, 'DWH Temperature', 'operating_data/sun_dhw_temp', SensorDeviceClass.TEMPERATURE),
-        RTBSensor(dc, 'External Temperature', 'operating_data/external_temp', SensorDeviceClass.TEMPERATURE),
-        RTBSensor(dc, 'Boiler Effect', 'operating_data/power_kw', SensorDeviceClass.POWER),
-        RTBSensor(dc, 'Total Consumption', 'consumption_data/counter', SensorStateClass.TOTAL_INCREASING), # state class STATE_CLASS_TOTAL_INCREASING
+        RTBBinarySensor(dc, 'Boiler Running', 'operating_data/power_pct', 'boiler_power_pct', BinarySensorDeviceClass.HEAT),
+        RTBBinarySensor(dc, 'Boiler Alarm', 'operating_data/off_on_alarm', 'boiler_state_off_on_alarm', BinarySensorDeviceClass.PROBLEM),
+        RTBSensor(dc, 'Boiler Temperature', 'operating_data/boiler_temp', 'boiler_temp', SensorDeviceClass.TEMPERATURE),
+        RTBSensor(dc, 'DWH Temperature', 'operating_data/sun_dhw_temp', 'dhw_temp', SensorDeviceClass.TEMPERATURE),
+        RTBSensor(dc, 'External Temperature', 'operating_data/external_temp', 'external_temp', SensorDeviceClass.TEMPERATURE),
+        RTBSensor(dc, 'Boiler Effect', 'operating_data/power_kw', 'power_kw', SensorDeviceClass.POWER),
+        RTBSensor(dc, 'Total Consumption', 'consumption_data/counter', 'pelletcounter', SensorStateClass.TOTAL_INCREASING), # state class STATE_CLASS_TOTAL_INCREASING
     ])
     _LOGGER.info(f"Sensor.py, sensors where added!")
 
@@ -43,12 +43,13 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class RTBSensor(CoordinatorEntity, SensorEntity):
     """Representation of an RTB sensor."""
 
-    def __init__(self, coordinator, name, client_key, device_class):
+    def __init__(self, coordinator, name, client_key, uid, device_class):
         """Initialize the sensor."""
         super().__init__(coordinator)
         self.client_key = client_key
         self._device_class = device_class
         self.sensorname = name
+        self.uid = uid
 
     @property
     def name(self):
@@ -56,6 +57,10 @@ class RTBSensor(CoordinatorEntity, SensorEntity):
         #_LOGGER.info(f"sensor.py (name) returning \"NBE {self.sensorname}\"")
         return f"NBE {self.sensorname}"
 
+    @property
+    def unique_id(self):
+        return self.uid
+    
     @property
     def state(self):
         """Return the state of the sensor."""
@@ -72,12 +77,13 @@ class RTBSensor(CoordinatorEntity, SensorEntity):
 class RTBBinarySensor(CoordinatorEntity, BinarySensorEntity):
     """Representation of an RTB binary sensor."""
 
-    def __init__(self, coordinator, name, client_key, device_class):
+    def __init__(self, coordinator, name, client_key, uid, device_class):
         """Initialize the binary sensor."""
         super().__init__(coordinator)
         self.client_key = client_key
         self._device_class = device_class
         self.sensorname = name
+        self.uid = uid
 
     @property
     def name(self):
@@ -87,15 +93,19 @@ class RTBBinarySensor(CoordinatorEntity, BinarySensorEntity):
 
     @property
     def is_on(self):
-        _LOGGER.info("is_on called")
+        _LOGGER.debug("is_on called")
         """Return the state of the binary sensor."""
         s = self.coordinator.rtbdata.get(self.client_key)
-        _LOGGER.info(f"sensor.py RTBBinarySensor (is_on) value \"{s}\"")
+        _LOGGER.debug(f"sensor.py RTBBinarySensor (is_on) value \"{s}\"")
         if "power_pct" in self.client_key:
             return s != "0"
         if "off_on_alarm"  in self.client_key:
             return s == "2"    
         return s == "0"
+    
+    @property
+    def unique_id(self):
+        return self.uid
 
     @property
     def device_class(self):
